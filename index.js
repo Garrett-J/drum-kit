06229523edce4264b53ee3a25dfd0d835337327c
Garrett Johnson <89829159+Garrett-J@users.noreply.github.com>
2022-06-09 13:23:54 -0700

    added uppercase functionality to makeSound and buttonAnimation

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Detects a button press
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -12,48 +11,52 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   });
 }
 
-
 //Detects a keyboard press
-
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
   buttonAnimation(event.key);
 });
 
 //Makes the sounds per which button is clicked or which keyboard key is pressed
-
 function makeSound(key) {
   switch (key) {
+    case "W":
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
+    case "A":
     case "a":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
+    case "S":
     case "s":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
+    case "D":
     case "d":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
+    case "J":
     case "j":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
+    case "K":
     case "k":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
+    case "L":
     case "l":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
@@ -64,14 +67,13 @@ function makeSound(key) {
   }
 }
 
-function buttonAnimation(currentKey){
-
-  var activeButton = document.querySelector("." + currentKey);
+//Adds pressed class to the current key being pressed
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey, currentKey);
 
   activeButton.classList.add("pressed");
 
-  setTimeout(function() {
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
